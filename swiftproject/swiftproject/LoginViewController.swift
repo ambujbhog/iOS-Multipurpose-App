237fe9d13c2cd8dad5eebaad3e6698c96299b9bc
Ambuj Bhog <ambujbhog@protonmail.com>
2020-07-24 21:40:47 -0400

    Initial commit

---
+++ swiftproject/swiftproject/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swiftproject
+//
+//  Created by snifle on 2020-07-23.
+//  Copyright © 2020 Ambuj Bhog. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()asad
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
